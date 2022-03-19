290e043de520fdb55c85e9b1ba1f8ae139ff87bd
Tuba Nur <yasatubanur@gmail.com>
2022-03-19 08:40:14 +0300

    personCell created

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Tuba Nur YAŞA on 18.03.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
